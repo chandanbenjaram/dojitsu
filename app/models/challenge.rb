a6edc0b02ca1a3ebb6a37358aa5332529f1f36f6
Chandan Benjaram <chandan.benjaram@gmail.com>
2011-10-07 00:16:52 -0700

    TA35 work changes

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,14 +4,22 @@ class Challenge
   include Mongoid::Timestamps::Updated
 
   field :title, :type=>String
+  field :description, :type=>String
+
+  field :dateStart, :type=>DateTime
+  field :dateEnd, :type=>DateTime
+  field :discipline, :type=>Array
+  field :participants, :type=>Array
+  field :rules, :type=>Array    
+  field :goals, :type=>Array        
+
   field :user_id, :type=>String
 
   embeds_many :tasks  
-
 
   validates_presence_of :title
   def user
     User.find(user_id)
   end              
-  
-end
+
+end                
