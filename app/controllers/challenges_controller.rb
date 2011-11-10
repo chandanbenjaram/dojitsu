82597a024addbf55651204204ea746975b74bbeb
pravinmishea88 <pravinmishra88@gmail.com>
2011-11-10 23:08:01 +0530

    worked on recursivelyembeds_may

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -36,9 +36,10 @@ class ChallengesController < ApplicationController
   def create
     
     #raise params.inspect
+    # INVITEE USER ID
+    @user_id = ["sriram@gmail.com","venkat@gmail.com","Suresh@gmail.com","sukendhar@gmail.com"]
     
     @ch = Challenge.new(params[:challenge])
-    
     @ch_st_date = params[:ch_st_date]
     @st_p_val1 = params[:st_value1]
     @st_p_val = params[:st_value]
@@ -56,12 +57,12 @@ class ChallengesController < ApplicationController
     unless @so_who_win.blank?
       #raise "soc"
       if @ch_st_date == "#ch_st_dat" and  @ch_ed_date == "#ch_ed_dat"
-        @challenge = Challenge.create!(:title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
+        @challenge = Challenge.new(:user_id => current_user.id, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
           :start_point => PointDateType.new(:value => @st_p_val1), \
           :end_point => PointDateType.new(:value => @ed_p_val1), \
           :social_type => ChallengeSocialType.new(:who_win => @so_who_win, :how_many_winners => @so_how_many_winner))
       else
-        @challenge = Challenge.create!(:title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
+        @challenge = Challenge.new(:user_id => current_user.id, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
           :start_point => PointNumberType.new(:value => @st_p_val, :label=> @st_p_leb), \
           :end_point => PointNumberType.new(:value => @ed_p_val, :label=>@ed_p_leb), \
           :social_type => ChallengeSocialType.new(:who_win => @so_who_win, :how_many_winners => @so_how_many_winner))
@@ -69,18 +70,25 @@ class ChallengesController < ApplicationController
     else
       #raise "per"
       if @ch_st_date == "#ch_st_dat" and  @ch_ed_date == "#ch_ed_dat"
-        @challenge = Challenge.create!(:title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
+        @challenge = Challenge.new(:user_id => current_user.id, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
           :start_point => PointDateType.new(:value => @st_p_val1), \
           :end_point => PointDateType.new(:value => @ed_p_val1), \
           :personal_type => ChallengePersonalType.new(:who_win => @pr_who_win))
       else
-        @challenge = Challenge.create!(:title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
+        @challenge = Challenge.new(:user_id => current_user.id, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
           :start_point => PointNumberType.new(:value => @st_p_val, :label=> @st_p_leb), \
           :end_point => PointNumberType.new(:value => @ed_p_val, :label=>@ed_p_leb), \
           :personal_type => ChallengePersonalType.new(:who_win => @pr_who_win))
       end
     end
-   
+     
+   @user_id.each do |userid|
+      @challenge.child_challenges.build(:user_id => userid, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
+          :start_point => PointNumberType.new(:value => @st_p_val, :label=> @st_p_leb), \
+          :end_point => PointNumberType.new(:value => @ed_p_val, :label=>@ed_p_leb), \
+          :personal_type => ChallengePersonalType.new(:who_win => @pr_who_win))
+   end
+    @challenge.save!
     render  :action => "show", :notice => "Challenge created!"   
   end
 
