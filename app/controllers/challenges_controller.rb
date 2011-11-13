5a369b67f7b98cb8a825c99a4175e19ef61495db
Chandan Benjaram <chandan.benjaram@gmail.com>
2011-11-13 01:59:08 -0800

    social challenge creation work based on social invitations modal & refactor challenge creation flow

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -3,7 +3,7 @@ class ChallengesController < ApplicationController
   before_filter :find_challenge, :only => [:show, :edit, :update, :destroy]
   #on_spot_edit  is the gem to edit the data on spot 
   can_edit_on_the_spot
-  
+
   def index
     @title = "Challenges"
     @no_of_row = Challenge.all.count
@@ -11,9 +11,9 @@ class ChallengesController < ApplicationController
   end
 
   def show
-      @challenge = Challenge.find(params[:id])	
+    @challenge = Challenge.find(params[:id])	
   end
-  
+
   def show_soc
     @challenge = Challenge.find(params[:id])
     @org = User.find(:all,:conditions => ["id=?",@challenge.user_id]).first
@@ -21,30 +21,30 @@ class ChallengesController < ApplicationController
   end
 
   def show_per
-      @challenge = Challenge.find(params[:id])
-      
-      #@challenge.task_attributes.each do |tsk|
-      #  raise tsk.name.inspect
-      #end
-      
-      
-      #@challenge.task_attributes.each do |ts|
-      #  ts.each do |sd|
-      #    raise sd[4].inspect
-      #  end
-      #end
+    @challenge = Challenge.find(params[:id])
 
-      #@challenge.social_type.type  
-      #if @challenge.social_challenge.type and @challenge.social_challenge.type!='0' and @challenge.social_challenge.type!=nil 
-      ch = @challenge.social_type.type 
-      #render :text => ch and return
-      if ch == ChallengeSocialType and ch!= ChallengeType and ch!=ChallengePersonalType
-        render "show_soc" and return 
-      else 
+    #@challenge.task_attributes.each do |tsk|
+    #  raise tsk.name.inspect
+    #end
+
+
+    #@challenge.task_attributes.each do |ts|
+    #  ts.each do |sd|
+    #    raise sd[4].inspect
+    #  end
+    #end
+
+    #@challenge.social_type.type  
+    #if @challenge.social_challenge.type and @challenge.social_challenge.type!='0' and @challenge.social_challenge.type!=nil 
+    ch = @challenge.social_type.type 
+    #render :text => ch and return
+    if ch == ChallengeSocialType and ch!= ChallengeType and ch!=ChallengePersonalType
+      render "show_soc" and return 
+    else 
       render "show_per"
     end
   end
-  
+
   def new
     @challenge = Challenge.new
     1.times {@challenge.tasks.build} 
@@ -52,66 +52,67 @@ class ChallengesController < ApplicationController
   end
 
   def create
-    # debugger
+
     #raise params.inspect
     #INVITEE USER ID
-    @user_id = ["sriram@gmail.com","venkat@gmail.com","Suresh@gmail.com","sukendhar@gmail.com"]
-    
+    # @user_id = ["sriram@gmail.com","venkat@gmail.com","Suresh@gmail.com","sukendhar@gmail.com"]
+
     @ch = Challenge.new(params[:challenge])
     @ch_st_date = params[:ch_st_date]
     @st_p_val1 = params[:st_value1]
     @st_p_val = params[:st_value]
     @st_p_leb = params[:st_label]
-    
+
     @ch_ed_date = params[:ch_ed_date]
     @ed_p_val1 = params[:ed_value1]
     @ed_p_val = params[:ed_value]
     @ed_p_leb = params[:ed_label]
-    
+
     @so_who_win = params[:soc_who_win]
     @so_how_many_winner = params[:soc_how_many_winner]
     @pr_who_win = params[:per_who_win]
-    
+
     unless @so_who_win.blank?
       #raise "soc"
-      if @ch_st_date == "#ch_st_dat" and  @ch_ed_date == "#ch_ed_dat"
-        @challenge = Challenge.new(:user_id => current_user.id, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
-          :start_point => PointDateType.new(:value => @st_p_val1), \
-          :end_point => PointDateType.new(:value => @ed_p_val1), \
-          :social_type => ChallengeSocialType.new(:who_win => @so_who_win, :how_many_winners => @so_how_many_winner))
-      else
-        @challenge = Challenge.new(:user_id => current_user.id, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
-          :start_point => PointNumberType.new(:value => @st_p_val, :label=> @st_p_leb), \
-          :end_point => PointNumberType.new(:value => @ed_p_val, :label=>@ed_p_leb), \
-          :social_type => ChallengeSocialType.new(:who_win => @so_who_win, :how_many_winners => @so_how_many_winner))
+      @challenge = Challenge.new(:user_id => current_user.id, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
+      :social_type => ChallengeSocialType.new(:who_win => @so_who_win, :how_many_winners => @so_how_many_winner)) do |new_challenge|
+        if @ch_st_date == "#ch_st_dat" and  @ch_ed_date == "#ch_ed_dat"
+          new_challenge.start_point =  PointDateType.new(:value => @st_p_val1)
+          new_challenge.end_point = PointDateType.new(:value => @ed_p_val1) 
+        else
+          new_challenge.start_point =  PointNumberType.new(:value => @st_p_val, :label=> @st_p_leb)
+          new_challenge.end_point = PointNumberType.new(:value => @ed_p_val, :label=>@ed_p_leb) 
+        end
+
+        if !params[:invitees].nil?
+          params[:invitees].split(",").each do |invitee|
+            new_challenge.child_challenges.build(:user_id => invitee, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
+            :start_point => PointNumberType.new(:value => @st_p_val, :label=> @st_p_leb), \
+            :end_point => PointNumberType.new(:value => @ed_p_val, :label=>@ed_p_leb), \
+            :personal_type => ChallengePersonalType.new(:who_win => @pr_who_win))
+          end      
+        end        
       end
     else
       #raise "per"
-      if @ch_st_date == "#ch_st_dat" and  @ch_ed_date == "#ch_ed_dat"
-        @challenge = Challenge.new(:user_id => current_user.id, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
-          :start_point => PointDateType.new(:value => @st_p_val1), \
-          :end_point => PointDateType.new(:value => @ed_p_val1), \
-          :personal_type => ChallengePersonalType.new(:who_win => @pr_who_win))
-      else
-        @challenge = Challenge.new(:user_id => current_user.id, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
-          :start_point => PointNumberType.new(:value => @st_p_val, :label=> @st_p_leb), \
-          :end_point => PointNumberType.new(:value => @ed_p_val, :label=>@ed_p_leb), \
-          :personal_type => ChallengePersonalType.new(:who_win => @pr_who_win))
-      end
+      @challenge = Challenge.new(:user_id => current_user.id, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
+      :personal_type => ChallengePersonalType.new(:who_win => @pr_who_win)) do |new_challenge|
+        if @ch_st_date == "#ch_st_dat" and  @ch_ed_date == "#ch_ed_dat"
+          new_challenge.start_point =  PointDateType.new(:value => @st_p_val1)
+          new_challenge.end_point = PointDateType.new(:value => @ed_p_val1) 
+        else
+          new_challenge.start_point =  PointNumberType.new(:value => @st_p_val, :label=> @st_p_leb)
+          new_challenge.end_point = PointNumberType.new(:value => @ed_p_val, :label=>@ed_p_leb) 
+        end
+      end            
     end
-     
-   @user_id.each do |userid|
-      @challenge.child_challenges.build(:user_id => userid, :title => @ch.title, :description => @ch.description, :task_attributes => @ch.task_attributes, \
-          :start_point => PointNumberType.new(:value => @st_p_val, :label=> @st_p_leb), \
-          :end_point => PointNumberType.new(:value => @ed_p_val, :label=>@ed_p_leb), \
-          :personal_type => ChallengePersonalType.new(:who_win => @pr_who_win))
-   end
+
     @challenge.save!
     redirect_to :action => "index", :notice => "Challenge created!"   
   end
 
   def edit
-  	#raise params.inspect
+    #raise params.inspect
     @challenge = Challenge.find(params[:id])
   end   
 
@@ -127,7 +128,7 @@ class ChallengesController < ApplicationController
     @challenge.destroy
     redirect_to :action => 'index'
   end  
-  
+
   def invite_frd
     @invitor_email = "pravin@gmail.com"
     @invitee_challenge_id = "4eb992f2af812d03000000b5"
@@ -135,7 +136,7 @@ class ChallengesController < ApplicationController
     @invitee_first_name = ["Sri Ram","Venkat","Suresh","Sukendhar"]
     @invitee_last_name = ["Kappor","Patlola","Mahadevan","Reddy"]
     @status = ["ACCEPTED","PENDING","ACCEPTED","DECLINED"]
-    
+
     @flag_fn = 0
     @flag_ln = 0
     @new_fn = []
@@ -143,9 +144,9 @@ class ChallengesController < ApplicationController
     #@id = "4eb922487c1bd8085c000053"
     #@challenge_copy = Challenge.find(@id)
     #@challenge_invitee = ChallengeInvitation.create(:invitee_challenge_id=>@invitee_challenge_id, :invitor_email=>@invitor_email, :invitees =>[Invitee.new(:invitee_email=>"sriram@gmail.com", :invitee_first_name=>"Sri ram", :invitee_last_name=>"Kapoor", :status =>"Accepted", :challenge=>Challenge.new(:title=>"ch001",:description=>"ch001 task is testing challenge")), Invitee.new(:invitee_email=>"venkat@gmail.com", :invitee_first_name=>"Venkat", :invitee_last_name=>"Reddy", :status =>"Pending")])
-    
+
     @challenge_invitee = ChallengeInvitation.create(:invitee_challenge_id=>@invitee_challenge_id, :invitor_email=>@invitor_email)
-    
+
     #@challenge_invitee.invitees.push([Invitee.new(:invitee_email =>"sriram@gmail.com")])
     @invitee_email.each_with_index do |in_email,em_index|
       @invitee_first_name.each_with_index do |in_fname,fn_index|
@@ -164,49 +165,49 @@ class ChallengesController < ApplicationController
       end
       @challenge_invitee.invitees.push([Invitee.new(:invitee_email =>in_email, \
         :invitee_first_name =>@new_fn[em_index], :invitee_last_name => @new_ln[em_index], :challenge=>Challenge.find(@invitee_challenge_id) )])
+      end
+
+      #APPEND CHALLGENG TO INVITEE WHO ACCEPTED REQUEST
+      #@id = "4eb922487c1bd8085c000053" 
+      #@challenge_copy = Challenge.find(@id)
+      #@challenge_invitee.invitees.push(:challenge=>[Challenge.new(:title =>"as")])
+      #@status.each_with_index do |st_value, st_index|
+      #  if st_value == "ACCEPTED"
+      #    #raise Challenge.find(@id).inspect
+      #    @challenge_invitee.invitees.push([Challenge.find(@id)]) 
+      #  else
+      #    raise "not works!!!!!!!!!!!"
+      #  end
+      #end
+
+      raise "CHECK DATABASE TABE 'challenge_invitations' for data"
     end
-    
-    #APPEND CHALLGENG TO INVITEE WHO ACCEPTED REQUEST
-    #@id = "4eb922487c1bd8085c000053" 
-    #@challenge_copy = Challenge.find(@id)
-    #@challenge_invitee.invitees.push(:challenge=>[Challenge.new(:title =>"as")])
-    #@status.each_with_index do |st_value, st_index|
-    #  if st_value == "ACCEPTED"
-    #    #raise Challenge.find(@id).inspect
-    #    @challenge_invitee.invitees.push([Challenge.find(@id)]) 
-    #  else
-    #    raise "not works!!!!!!!!!!!"
-    #  end
-    #end
-    
-    raise "CHECK DATABASE TABE 'challenge_invitations' for data"
-  end
-  
-  def invitee_accepted_req
-    raise "aaaaaaaaa"
-  end
-  
-  def challenge_comp
-    #Challenge.where(:_id => "4ebe067b7c1bd8023800006d").update(:is_complete => 1)
-    #@id =params[:id]
-    #raise @sdf = Challenge.update_attribute(:is_complete => 1).where(:id => @id).inspect
-	@sdf = Challenge.where(:_id => params[:id]).update(:is_complete => 1)
-	redirect_to :action => "index"
-  end
-  
-  def my_challenge
-    @my_total_challenge = Challenge.all.count
-    @my_all_ch = Challenge.all
-    @my_all_ch.each do |sd|
-      @org = User.find(:all,:conditions =>["id = ?",sd.user_id]).first
+
+    def invitee_accepted_req
+      raise "aaaaaaaaa"
     end
+
+    def challenge_comp
+      #Challenge.where(:_id => "4ebe067b7c1bd8023800006d").update(:is_complete => 1)
+      #@id =params[:id]
+      #raise @sdf = Challenge.update_attribute(:is_complete => 1).where(:id => @id).inspect
+      @sdf = Challenge.where(:_id => params[:id]).update(:is_complete => 1)
+      redirect_to :action => "index"
+    end
+
+    def my_challenge
+      @my_total_challenge = Challenge.all.count
+      @my_all_ch = Challenge.all
+      @my_all_ch.each do |sd|
+        @org = User.find(:all,:conditions =>["id = ?",sd.user_id]).first
+      end
+    end
+
+    protected
+
+    def find_challenge
+      @challenge = Challenge.all
+    end
+
   end
-      
-  protected
-  
-  def find_challenge
-    @challenge = Challenge.all
-  end
-  
-end
 
