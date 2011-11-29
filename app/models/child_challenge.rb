f4654bdb52bde141be163b95a9926615f62bc877
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2011-11-29 19:23:05 +0530

    pull has_many relation changes

--- app/models/child_challenge.rb
+++ app/models/child_challenge.rb
@@ -1,10 +1,10 @@
 class ChildChallenge < Challenge   
   include Mongoid::Document 
   
-    include Rails.application.routes.url_helpers
+  include Rails.application.routes.url_helpers
   include Mongoid::Timestamps::Created
   include Mongoid::Timestamps::Updated
-    field :title, :type=>String 
+  field :title, :type=>String 
     
   belongs_to :challenge  
 
