e4e9976b022030f02d5265307a0cc19d870f853f
sriram.in20@gmail.com <sriram.in20@gmail.com>
2011-12-07 22:46:55 +0530

    worked on mydojo css

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,11 @@ Dojitsu::Application.routes.draw do
   # static pages
   match '/aboutus', :to=>'static#aboutus'
   match '/contactus', :to=>'static#contactus'
-  resources :user_connections  
+  resources :user_connections   do
+    collection do
+      put :update_attribute_on_the_spot
+    end
+  end
   resources :messages do
     collection do
       get "storemessage"
