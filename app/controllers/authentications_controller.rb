d7c9ce3995a041910cff25c28110cdb6cd7ae33c
suresh <systemhack95@gmail.com>
2012-01-01 11:31:26 +0530

    saturday first pull

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -57,7 +57,7 @@ class AuthenticationsController < ApplicationController
             if user.save!
               flash[:notice] = "New user signed in successfully." 
               sign_in_and_redirect(:user, user)
-              session[:isNewChallenge] = "isNew"
+              session[:isNewUser] = "isNew"
 			
             else
               redirect_to new_user_registration_path
