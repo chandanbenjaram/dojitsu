ca8e229c2d76a32595aea8242299fb42bfda0f65
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2012-01-21 16:50:21 +0530

    worked on message_details page 06

--- app/assets/javascripts/messageJs.js
+++ app/assets/javascripts/messageJs.js
@@ -17,3 +17,23 @@ $(document).ready(function(){
           });
     });
 });
+
+$(document).ready(function(){
+    $(".iMessageNew").click(function(){
+        //alert("Maisa");
+        $.colorbox({
+              opacity: 0.40,
+              transition: 'none',
+              speed: 200,
+              width: 450,              
+              scrolling: true,
+              inline:true, 
+              href:"#sendNewMessage",
+              title: '',
+              overlayClose: true,
+              onComplete: function () {
+                  // $(this).initFunctions(); // if you want to reinit another colorbox, you should pass the element as an argument to initFunctions rather than calling this as a blanket initializer for all colorboxes.
+              }              
+          });
+    });
+});
