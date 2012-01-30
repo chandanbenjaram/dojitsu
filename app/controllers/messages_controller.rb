ef8c39832be393e45d1dc72c7f8bbc8999186e4c
Chandan Benjaram <chandan.benjaram@gmail.com>
2012-01-30 02:51:27 -0800

    messaging bug fix & code cleanup

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,20 @@
 class MessagesController < ApplicationController
   before_filter :authenticate_user!, :except =>[:storemessage] 
   
-	def storemessage
-		@name = params[:messageDetails][:name]
-		@to = params[:messageDetails][:recipients]
-		@from = params[:messageDetails][:author_id]
-		@body = params[:messageDetails][:body]
+	def storemessage   
+	  debugger
+	 	@from = params[:from]
+		@to = params[:to]
+		@subject = params[:subject]
+		@body = params[:body]  
 		@to.each do |messageTo|
 			unless messageTo == @from  
 				Message.create(:to => messageTo, :from => @from, :body => @body, :subject => @name)
 			end	
 		end
+		
+		# ajax response
+    
 	end
   
   def newmessage 
--- vendor/ruby/1.9.1/gems/rubber-1.15.0/lib/generators/vulcanize/templates/mysql_proxy/config/rubber/common/mysql-proxy.lua
+++
@@ -1,5 +0,0 @@
-<%
-  @path = "/etc/mysql/mysql-proxy.lua"
-%>
-
--- we could put a lua script here if we needed to customize mysql-proxy behavior
