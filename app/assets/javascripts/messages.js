87596733bc4265e3b453f65e26373059869bca57
Chandan Benjaram <chandan.benjaram@gmail.com>
2012-02-01 21:35:02 -0800

    alpha release version with minor fixes

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -10,23 +10,26 @@
 				text: message,
 				message: message,
 				data: message,
-				}, function(response){
+			   	}, function(response){
 					$.dojitsu.messageCallBack(response, subject); 
 					if(callback && typeof callback === 'function'){
 						callback(response);
-						};
+					};
 					})	
-				},
-				messageCallBack : function(response){  				             
-					window.fbAsyncInit();
-					FB.api({
-						method: 'fql.multiquery',
-						return_ssl_resources: 1,
-						queries: {
-							"mThreadQ" : "SELECT thread_id, recipients, subject, snippet,updated_time,viewer_id FROM thread WHERE folder_id = 1 order by updated_time asc limit 1", 
-							"mMessageQ": "SELECT author_id, created_time, body, attachment,viewer_id FROM message WHERE thread_id IN (SELECT thread_id from #mThreadQ)  order by created_time desc limit 1"
-							},
-							}, function(response){
+					},
+					messageCallBack : function(response){  	
+						if(response === null){ 
+							return; 
+						}  
+						window.fbAsyncInit();
+						FB.api({
+							method: 'fql.multiquery',
+							return_ssl_resources: 1,
+							queries: {
+								"mThreadQ" : "SELECT thread_id, snippet, recipients, snippet FROM thread WHERE folder_id='1' ORDER BY updated_time DESC LIMIT 1", 
+								"mMessageQ": "SELECT author_id, created_time, body, attachment, viewer_id FROM message WHERE thread_id IN(SELECT thread_id from #mThreadQ) ORDER BY created_time DESC LIMIT 1"
+								},
+								}, function(response){
 								if(response && response[0] && response[1]){
 									var messageLog = $.extend({}, response[0].fql_result_set[0], response[1].fql_result_set[0]);  
 									// call rails to put entry into messages table
