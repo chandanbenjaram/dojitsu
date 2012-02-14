7d188dd4f6ce91a8c1cf916a1a258604a5d2afea
sukendhar <sukendhar555@gmail.com>
2012-02-14 16:18:23 +0530

    HTML5&css3 changes in Challenges pages

--- app/assets/javascripts/dashboard_againstmyself.js
+++ app/assets/javascripts/dashboard_againstmyself.js
@@ -44,3 +44,9 @@ $(document).ready(function()
 			$("#myTable").tablesorter(); 
 		} 
 	); 
+	
+$(document).ready(function() { 
+	
+		$("#myTable").tablesorter( {sortList: [[0,0],[1,0]] } ); 
+		
+	}); 
